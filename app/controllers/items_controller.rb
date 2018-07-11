ddc9c18dd59dbbe8b9a56fcb0c5c55d105d0d8b7
aya.aya <aya.aya@techacademy.jp>
2018-07-11 09:44:33 +0000

    want and have

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,14 @@ class ItemsController < ApplicationController
     @items = []
     
     @keyword = params[:keyword]
-    if @keyword.present?
+    if @keyword
       results = RakutenWebService::Ichiba::Item.search({
         keyword: @keyword,
         imageFlag: 1,
         hits: 20,
       })
       results.each do |result|
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
